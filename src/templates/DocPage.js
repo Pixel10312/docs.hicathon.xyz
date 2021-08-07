5f212afdbd490e43b8ab866078ea697e235d48c6
Pixel10312 <61020102+Pixel10312@users.noreply.github.com>
2021-08-07 16:48:40 -0400

    update

--- src/templates/DocPage.js
+++ src/templates/DocPage.js
@@ -16,7 +16,7 @@ const DocPage = ({
         gcms: { article },
         },
     }) => {
-        const [active, setActive] = useState(true)
+        const [active, setActive] = useState(false)
 
         useEffect(() => {
             if (window.outerWidth < 1025) {
@@ -29,17 +29,21 @@ const DocPage = ({
                 <Helmet>
                     <title>{'hicathon docs | ' + article.name }</title>
                 </Helmet>
-                <div className="page__menu">
-                    <Logo />
-                    <div className={active ? 'logo__trigger' : 'trigger__active'} onClick={() => setActive(!active)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#000000"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
-                        </div>
-                        {active && (
-                            <div className="page__menu__content">
-                                <Event />
-                            </div>  
-                        )}
+                <div className={active ? 'page__menu__active' : 'page__menu'}>
+                <Logo />
+                <div className={active ? 'logo__trigger' : 'trigger__active'} onClick={() => setActive(!active)}>
+                    {active ? (
+                        <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14l-6-6z"/></svg>
+                    ) : (
+                        <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#000000"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
+                    )}
                 </div>
+                {active && (
+                    <div className="page__menu__content">
+                        <Event />
+                    </div>  
+                )}                  
+            </div>
                 <div className="page__content">
                     <React.Fragment>
                         <h1 className="article__title">{article.name}</h1>
